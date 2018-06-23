d833849eed9f29b048db2413d8f5f49ba3dd2022
pkbgit <prakashkbehera@gmail.com>
2018-06-23 16:47:48 +0530

    TestJava2.java created first time

---
+++ posbravo2/src/com/pkb/pkg/TestJava2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.pkb.pkg;
+
+/**
+ * @author PRAKASH
+ *
+ */
+public class TestJava2 {
+
+	/**
+	 * 
+	 */
+	public TestJava2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
