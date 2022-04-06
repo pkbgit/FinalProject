791502be95e62921a28e1173ddfc24ccbc318522
Prakash <prakashkbehera@gamil.com>
2022-04-06 09:30:12 +0530

    AdditionTest class added

---
+++ posbravo2/src/com/pkb/pkg/AdditionTest.java
@@ -0,0 +1,17 @@
+package com.pkb.pkg;
+
+public class AdditionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c;
+		
+		c = a + b;
+		
+		System.out.println("Addition of a and b is: "+c);
+
+	}
+
+}
