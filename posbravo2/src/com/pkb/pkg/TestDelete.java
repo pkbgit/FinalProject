9bb11ba1bf8082915ea28f63eeb9c233e2dab8c3
Prakash <prakashkbehera@gmail.com>
2022-09-09 16:46:15 +0530

    TestDelete class added

---
+++ posbravo2/src/com/pkb/pkg/TestDelete.java
@@ -0,0 +1,10 @@
+package com.pkb.pkg;
+
+public class TestJava3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Heloo India. I love India.");
+	}
+
+}
